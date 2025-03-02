fd77566dbd422a834f83b900185e84b58602fca2
Bernardo Diniz <bernardoldiniz04@gmail.com>
2025-03-01 22:26:04 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { ThemeProvider } from "styled-components"
+import { defaultTheme } from "./styles/themes/default"
+import { GlobaStyle } from "./styles/globals"
+import { Transactions } from "./pages/Transactions"
+
+
+export function App() {
+  
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobaStyle />
+
+        <Transactions/>      
+    </ThemeProvider>
+  )
+}
+
